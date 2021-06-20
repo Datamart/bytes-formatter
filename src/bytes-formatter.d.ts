e89e4f97b67e34bf43d4c75226998e33bbb863dd
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-06-20 10:44:04 -0700

    Added typescript declaration file.

---
+++ src/bytes-formatter.d.ts
@@ -0,0 +1,11 @@
+// typescript 
+
+/**
+ * Formats given <code>bytes</code> to human friendly format.
+ * @param {number} bytes The bytes to be formatted.
+ * @return {string} The formatted bytes as string.
+ */
+declare function formatBytes(bytes:number):string;
+
+// Exposed public method.
+export default formatBytes;
